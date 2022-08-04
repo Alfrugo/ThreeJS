492d055eb9565463058588ee67237da363abeaa6
Alfrugo <adiez@mtm-inc.net>
2022-08-04 11:42:32 -0500

    section burst animation with green sock

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import gsap from 'gsap'
 
 /**
  * Debug
@@ -170,9 +171,31 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 */
 
 let scrollY = window.scrollY
+let currentSection = 0
 
 window.addEventListener('scroll', () => {
     scrollY = window.scrollY
+
+    const newSection = Math.round(scrollY / sizes.height)
+
+
+    if (newSection  != currentSection)
+    {
+        currentSection = newSection
+        // console.log('changed ', currentSection)
+
+        gsap.to(
+            sectionMeshes[currentSection].rotation,
+            {
+                duration: 1.5,
+                ease: 'power2.inOut',
+                x: '+=6',
+                y: '+=3',
+                z: '+=1.5'
+            }
+        )
+
+    }
 })
 
 /**
@@ -213,8 +236,8 @@ const tick = () =>
     // Animate meshes
     for(const mesh of sectionMeshes)
     {
-        mesh.rotation.x = elapsedTime * 0.1
-        mesh.rotation.y = elapsedTime * 0.12
+        mesh.rotation.x += deltaTime * 0.1
+        mesh.rotation.y += deltaTime * 0.12
     }
 
 
